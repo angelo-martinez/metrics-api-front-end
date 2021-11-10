6e89bea46cc9a9bacebd89828323aa6e4c02210c
Angelo Martinez <amart670@fiu.edu>
2021-11-09 22:58:14 -0500

    finished navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,12 +1,14 @@
 import Wrapper from '../Wrapper';
-import { bar } from './styles.module.css';
+import { bar, title, row } from './styles.module.css';
 
 const NavBar = () => {
   return (
     <nav className={bar}>
       <Wrapper>
-        <h1 className='title'>Metrics Admin</h1>
-        <button className='btn'>Add Metric</button>
+        <div className={row}>
+          <h1 className={title}>Metrics Admin</h1>
+          <button className='btn'>Add Metric</button>
+        </div>
       </Wrapper>
     </nav>
   );
