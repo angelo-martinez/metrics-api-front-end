acbe77ecd67d37c1fab587d3a6ca61b6caed5d4e
Angelo Martinez <amart670@fiu.edu>
2021-11-09 22:03:20 -0500

    addded onload request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState('');
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    };
+
+    fetch('http://localhost:5000/metrics', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(e => console.log(e))
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        initial commit
-      </header>
+      {JSON.stringify(data)}
     </div>
   );
 }
